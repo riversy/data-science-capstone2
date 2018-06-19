6bd18b1688d54437b67b2b8170d6582ffd5099ed
Ihar Haltsou <igor@ecomgems.com>
2018-06-19 10:21:04 +0300

    Starting with data cleaning

---
+++ utils/text_clear.R
@@ -0,0 +1,23 @@
+source('utils/requirements.R')
+
+
+clean_text_data <- function(text_corpus) {
+  
+  # 1. Split to sentences
+  
+  
+  
+  # 2. Text to lower
+  
+  # 3. Extract words only
+  
+  
+  # 4. Deal with 'you' 'll' (you will) 
+
+  # 5. Remove profanity words
+  
+  # 6. Concat it back to corpus again
+  
+  text_corpus
+
+}
